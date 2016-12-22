2ae99307f7fcc23fee26392e69c6abb68060200c
yhuangbl <yhuangbl@connect.ust.hk>
2016-12-22 01:44:36 -0500

    Delete dbconfig.js

--- routes/dbconfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user: "cis550group4",
-    password: "projectgroup4cis550",
-    connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)\
-(HOST= cis550group4.cjmvdchisgcg.us-east-1.rds.amazonaws.com)(PORT=1521))\
-(CONNECT_DATA=(SID=PENNTR)))"
-};
